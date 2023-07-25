77363677d1d394d029bafd0a24eb31dbb47db5c4
OoXooOx <107791774+OoXooOx@users.noreply.github.com>
2023-07-25 14:36:07 +0300

    Update AddReferalList.sol

--- AddReferalList.sol
+++ AddReferalList.sol
@@ -2,21 +2,25 @@
 
 pragma solidity 0.8.19;
 contract sample{
-    mapping(address => address[]) public referalList;
-    //refer=>referals
+    mapping(address => address[]) public referralList;
+    //refer=>referrals
 
     function addRefer(address refer) external {
-        uint256 found;
-        uint256 length = referalList[refer].length;
-        for (uint256 i = 0; i < length;) {
-            if (referalList[refer][i] == msg.sender) {
+        uint256 found; 
+        uint256 length = referralList[refer].length;
+        // we looking for match address, if our array of refferals already include it,
+        // then we do nothing
+        for (uint256 i = 0; i < length;) { 
+            if (referralList[refer][i] == msg.sender) {
                 found = 1;
                 break;
             }
             unchecked{++i;}
         }
-        if (found == 0) {
-            referalList[refer].push(msg.sender);
+        // if we NOT found match address in array, only then we add it
+        // for certain refer
+        if (found == 0) { 
+            referralList[refer].push(msg.sender);
         } 
     }
 }
