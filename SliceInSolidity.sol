765f854793726881e2222bc1835158e3b7c15f5a
OoXooOx <107791774+OoXooOx@users.noreply.github.com>
2023-10-12 20:48:27 +0300

    Update SliceInSolidity.sol

--- SliceInSolidity.sol
+++ SliceInSolidity.sol
@@ -6,7 +6,15 @@ contract sample {
     
     function getFirstPool(bytes calldata path) public pure returns (bytes calldata) {
         return path[:23];
-        // 0x04fb784d1be2b8e7079d34df3addd8bfcc2f0ccf002710c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
-        // 0x04fb784d1be2b8e7079d34df3addd8bfcc2f0ccf002710
+        // input - 0x04fb784d1be2b8e7079d34df3addd8bfcc2f0ccf002710c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
+        // result -0x04fb784d1be2b8e7079d34df3addd8bfcc2f0ccf002710
     }
+
+    function skipToken(bytes calldata path) public pure returns (bytes calldata) {
+        return path[23:]; // 20+3
+        // input - 0x04fb784d1be2b8e7079d34df3addd8bfcc2f0ccf002710c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
+        // result -                                                c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
+    }
+
+
 }
